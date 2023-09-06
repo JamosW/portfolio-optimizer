26eba2c6abab42708ebbead31f8222115695da58
James Wilson <104772245+JamosW@users.noreply.github.com>
2023-09-06 15:27:13 -0400

    removed uneeded files

--- app.py
+++ app.py
@@ -1,28 +1,57 @@
 from shiny import App, Inputs, Outputs, Session, render, ui, reactive
-from yahooquery import Ticker
+import shiny.experimental as x
 from datetime import date, timedelta
-from core_funcs import extra_params, stock_returns, get_params, portfolios, get_symbols, portfolios_plot
+from core_funcs import np, min_date_dfs, stock_returns, get_params, portfolios, portfolios_plot, get_tickers
+from shinyswatch import theme
+from re import sub, split
+from htmltools import css
 
 today = date.today()
+    
+def card(title, values, ispercent):
+    
+    cleaned_lists = sub(r'[\[\]\(\)]|[a-z]', '', str(values))
+    percentages = [str(np.round((np.float16(i) * 100), 2)) + "%" for i in split(' ', cleaned_lists) if i != '']
+    non_percent = sub(r'[\[\]]', '', str(values))
+    
+    toDisplay = []
+    if(ispercent):
+        toDisplay.append(percentages)
+    else:
+        toDisplay.append(non_percent)
+    
+    return(
+        x.ui.card(
+            x.ui.card_title(title,  style=css(font_weight="bold", color="#002147")),
+            x.ui.card_body(
+                ui.div(ui.h2(sub(r'[\[\]\(\),\']', '',str(toDisplay))))
+            ),
+            height = '130px'
+        )
+    )
+    
 
 app_ui = ui.page_fluid(
+    theme.cosmo(),
+    #ui.panel_conditional("input.calculate", x.ui.layout_column_wrap(1 / 4, *[value_box(title) for title in ["Optimal Weights", "Sharpe ratio", "three", "four"]])),
+    ui.output_ui("value_boxes"),
     ui.layout_sidebar(
         ui.panel_sidebar(
             ui.row(
                 ui.column(
-                4,
-                ui.input_select(id = "interval", label = "Interval",  choices = extra_params()["intervals"][-5:], width= "130px"), 
-                ui.input_date(id = "start", label = "Start", max = today, width = "130px"),
-                ui.output_ui("end")
+                4, 
+                ui.input_date(id = "start", label = "Start", max = today - timedelta(days=35), value = today - timedelta(days=35), width = "130px"),
+                ui.input_date(id = "end", label = "End", max = today, value = today, min = today, width = "130px")
             ),
             ui.column(
                 4,
-                ui.input_selectize("tickers", "Choose Ticker", choices = get_symbols(), multiple=True)
+                ui.input_selectize("tickers", "Choose Ticker", choices = get_tickers(), multiple=True)
                 
             ),
             ),
-            ui.input_slider("samples", "Number of Samples", min=1000, max=10000, value=5000, step = 200),
-            ui.input_action_button("calculate", "Calculate")
+            ui.input_slider("samples", "Number of Samples", min=1000, max=20000, value=5000, step = 200),
+            ui.input_action_button("calculate", "Calculate"),
+                        ui.output_text("txter")
         ),
         ui.panel_main(
             ui.output_plot("plot"),
@@ -33,45 +62,73 @@ app_ui = ui.page_fluid(
 
 def server(input: Inputs, output: Outputs, session: Session):
     
-    @reactive.Calc
+    #update the selectize order, to match the weights values, np.unique sorts names
+    @reactive.Effect
     @reactive.event(input.calculate)
-    def main_df():
-        #change later
-        dfs = Ticker(input.tickers(), asynchronous = True).history(interval=input.interval(), start = input.start(), end = "2021-01-07")
-        #the lowest date we can 
-        lower_bound = max([i for i in dfs.reset_index().groupby("symbol")["date"].min()])
-        
-        return dfs
-    
-    @output
-    @render.ui
-    @reactive.Calc
-    def end():
-        return ui.TagList(
-            ui.input_date(id = "end", label = "End", min = input.start(), value = input.start(), max = min([today, input.start() + timedelta(weeks = 300)]), width = "130px")
+    def _():
+        a = list(input.tickers())
+        a.sort()
+        ui.update_selectize(
+            "tickers",
+            selected = a
         )
+    
+    @reactive.Calc
+    def main_df():
+        lower_bound, dfs = min_date_dfs(symbols = input.tickers(), start = input.start(), end = input.end())
+
+        return dfs
+      
+    @output
+    @render.table
+    def table():
+        return(main_df())
     
     @reactive.Calc
     def portfolio_vis_data():
         df = stock_returns(main_df())
         params = get_params(df)
-        all_portfolios = portfolios(len(input.tickers()), params, stock_returns(main_df()), input.samples())
+        all_portfolios = portfolios(len(input.tickers()), params, df, input.samples())
         
         return all_portfolios
+    
+    @output
+    @render.ui
+    @reactive.event(input.calculate)
+    def value_boxes():
+        
+        portfolio_data = portfolio_vis_data()
+        optimal_weights, min_variance_weights = [np.round(i, 3) for i in [portfolio_data[3], portfolio_data[4]]]
+        
+        return ui.TagList(
+            x.ui.layout_column_wrap(1 / 4, *[card(title, val, perc) for title,val,perc in zip(["Optimal Weights", "Highest Sharpe Ratio", "Minimum Variance Portfolio", "Minimum Variance"], 
+                                                                                      [optimal_weights,
+                                                                                      np.float16(np.round(portfolio_data[2], 3)),
+                                                                                      min_variance_weights, portfolio_data[5]],
+                                                                                      [True] * 3 + [False]
+                                                                                      )])
+        )
     
     @output
     @render.plot
     @reactive.event(input.calculate)
     def plot():
-        return portfolios_plot(*portfolio_vis_data())
+        return portfolios_plot(*portfolio_vis_data()[0:2])
     
+    
+    @output
+    @render.text
+    @reactive.event(input.calculate)
+    def txter():
+        lower_bound, dfs = min_date_dfs(symbols = input.tickers(), start = input.start(), end = input.end())
+        
+        #conditional message if start date is lower than the lower bound
+        if input.start() < lower_bound - timedelta(days = 30):
+            return f"Lowest date all {len(input.tickers())} tickers share in common is {lower_bound}"
+        else:
+            return None
 
 app = App(app_ui, server)
 
 
 #document plt change
-#limit to 1 month, no need for select input
-#upload stock data to server
-#filter ticker name from start and end date range
-#pull stock data from from server
-#add boxes of value data
--- core_funcs.py
+++ core_funcs.py
@@ -1,28 +1,24 @@
 
-from yahooquery.base import _YahooFinance as yf
 import numpy as np
-import pandas as pd
 from itertools import combinations
-from pytickersymbols import PyTickerSymbols
 import matplotlib.pyplot as plt
 from yahooquery import Ticker
 
-def get_dfs(in_symbols, in_interval, in_start, in_end):
-    dfs = Ticker(in_symbols, asynchronous = True).history(interval=in_interval, start = in_start, end = in_end)
+
+
+
+def min_date_dfs(symbols, start, end):
+    
+    dfs = Ticker(symbols= symbols, asynchronous = True).history(interval="1mo", start = start, end = end)
         
-    #the lowest date we can go due to difference in stocks ipo
-    #lower_bound = max([i for i in dfs.reset_index().groupby("symbol")["date"].min()])
+    lower_bound = max([i for i in dfs.reset_index().groupby("symbol")["date"].min()])
     
-    return dfs
-
-def extra_params():
-    config = yf._CONFIG["chart"]
-    intervals = config["query"]["interval"]["options"]
+    filtered_dfs = dfs[dfs.index.get_level_values(1) >= lower_bound]
     
-    extras = {"intervals": intervals}
-    return extras
-
-def stock_returns(df) -> [pd.Series]:
+    return lower_bound, filtered_dfs
+    
+    
+def stock_returns(df):
     df_list = []
     for df in [df.loc[[i]] for i in np.unique(df.index.get_level_values(0))]:
         close = np.array(df.close)
@@ -36,7 +32,7 @@ def get_params(rets):
     means = [i.mean() for i in rets]
     errors_list = [(ret - mean) for mean,ret in zip(means, rets)]
     squared_errors = [errors**2 for errors in errors_list]
-    variances = [sum(sq_error)/len(sq_error) for mean,sq_error in zip(means, squared_errors)]
+    variances = [sum(sq_error)/len(sq_error) for sq_error in squared_errors]
     #might not need this
     standard_dev = np.sqrt(variances)
 
@@ -94,7 +90,7 @@ def portfolios(limit, params, df, samples):
     #covariance matrix
     covar = np.cov(df, ddof = 0)
 
-    #covariance unique
+    #covariance unique without diaganol values (variances)
     cov = np.unique(covar[np.where(~np.eye(covar.shape[0], dtype = bool))])
 
     weight_combos = list(map(lambda x: list(combinations(x, 2)), weights))
@@ -108,21 +104,84 @@ def portfolios(limit, params, df, samples):
     #expected return
     e_r = np.sum(weights * means, axis = 1)
     
-    #portfolio variance, may not be correct
+    #portfolio variance
     pv = np.sum(weights**2 * var, axis = 1) + p2 
     #* (cor * np.prod(std)))
 
     #pv = np.sum(weights**2 * var, axis = 1) + 2 * [list(combinations(i, r = 2)) for i in randos]
     p_stdv = np.sqrt(pv)
 
-    #modified sharpe ratio
-    s_r = e_r / p_stdv
-    return p_stdv, e_r
-
-def get_symbols():
+    #modified sharpe ratios
+    s_rs = e_r / p_stdv
     
-    stock_data = PyTickerSymbols()
-    s_and_p = stock_data.get_all_stocks()
-    symbols = [i["symbol"] for i in s_and_p if i["symbol"] is not None]
-    symbols.sort()
-    return symbols
+    #indexed optimal values and minimum variance
+    optimals= np.where(s_rs == np.max(s_rs))
+    min_variance_ind = np.where(p_stdv == np.min(p_stdv))
+   
+    #max sharpe ratio
+    s_r = s_rs[optimals]
+    
+    #minimum variance gained from the index
+    min_variance = p_stdv[min_variance_ind]
+    
+    #max sharpe ratio weights and min variance weight
+    sharpe_weights, min_variance_weights = [weights[i] for i in [optimals, min_variance_ind]]
+    
+    return p_stdv, e_r, s_r, sharpe_weights, min_variance_weights, min_variance
+
+
+def get_tickers():
+    
+    return(['A','AAL','AAP','AAPL','ABBV','ABC','ABT','ACA','ACGL','ACN','ADBE','ADI'
+,'ADM','ADP','ADSK','ADTN','AEE','AEP','AES','AFL','AGOAF','AGS','AHT'
+,'AIG','AIR','AIZ','AJG','AKAM','ALB','ALC','ALGN','ALK','ALL','ALLE'
+,'ALRS','ALV','AMAT','AMCR','AMD','AME','AMGN','AMP','AMS','AMT','AMZN'
+,'ANET','ANSS','AOF','AON','AOS','APA','APAM','APD','APH','APTV','ARE'
+,'ARGX','ASM','ASML','ATO','ATVI','AVB','AVGO','AVY','AWK','AXP','AZN'
+,'AZO','BA','BAC','BAX','BB','BBVA','BBWI','BBY','BDX','BEN','BF-B','BIDU'
+,'BIIB','BIO','BK','BKNG','BKR','BKT','BLK','BME','BMY','BN','BOIVF'
+,'BOSS','BR','BRK-B','BRO','BSL','BSX','BWA','BXP','C','CAG','CAH','CARM'
+,'CAT','CB','CBOE','CBRE','CCI','CCL','CDAY','CDNS','CDW','CE','CF','CFG'
+,'CFR','CGGYY','CGUSY','CHD','CHRW','CHTR','CI','CINF','CL','CLX','CMA'
+,'CMCSA','CME','CMG','CMI','CMS','CNA','CNC','CNP','COF','COO','COP'
+,'COST','CPB','CPG','CPRT','CPT','CRH','CRL','CRM','CRWD','CSCO','CSGP'
+,'CSX','CTAS','CTLT','CTRA','CTSH','CTVA','CVS','CVX','CZR','D','DAL'
+,'DDOG','DE','DFS','DG','DGX','DHI','DHR','DIS','DISH','DLR','DLTR','DOCU'
+,'DOV','DOW','DPZ','DRI','DSM','DTE','DUK','DVA','DVN','DXC','DXCM','EA'
+,'EBAY','ECL','ED','EDV','EFX','EIX','EL','ELV','EMN','EMR','ENG','ENPH'
+,'ENR','EOG','EPAM','EQIX','EQR','EQT','ERMAY','ES','ESS','ETN','ETR'
+,'ETSY','EUTLF','EVRG','EVT','EW','EXC','EXPD','EXPE','EXR','EZM','F'
+,'FANG','FAST','FCX','FDS','FDX','FE','FFIV','FIS','FITB','FIVE','FLT'
+,'FMC','FOXA','FRA','FRES','FRT','FTK','FTNT','FTV','GBF','GD','GE','GEN'
+,'GILD','GIS','GL','GLPG','GLW','GM','GNRC','GOOGL','GPC','GPN','GRF'
+,'GRMN','GRUPF','GS','GSEFF','GSK','GWW','HAL','HAS','HBAN','HCA','HD'
+,'HEI','HES','HIG','HII','HLT','HOLX','HON','HPE','HPQ','HRL','HSIC','HST'
+,'HSY','HUM','HWM','IAG','IBM','ICE','IDR','IDXX','IEX','IFF','IHG','III'
+,'ILMN','INCY','INTC','INTU','INVH','IP','IPG','IQV','IR','IRM','ISP'
+,'ISRG','IT','ITP','ITW','IVZ','JBHT','JCDXF','JCI','JD','JKHY','JNJ'
+,'JNPR','JPM','K','KDP','KEY','KEYS','KHC','KIM','KLAC','KMB','KMI','KMX'
+,'KO','KR','L','LAND','LDOS','LEG','LEN','LH','LHX','LIN','LKQ','LLY'
+,'LMT','LNC','LNT','LOGN','LOW','LRCX','LULU','LUMN','LUV','LVS','LW'
+,'LYB','LYV','MA','MAA','MAR','MAS','MBG','MC','MCD','MCHP','MCK','MCO'
+,'MDLZ','MDT','MEIYF','MELI','MET','META','MGM','MGNT','MHK','MKC','MKTX'
+,'MLM','MMC','MMM','MNST','MO','MOH','MOR','MOS','MPC','MPWR','MRK','MRNA'
+,'MRO','MRVL','MS','MSCI','MSFT','MSI','MT','MTB','MTCH','MTD','MTX','MU'
+,'NCLH','NDAQ','NDSN','NEE','NEM','NFLX','NI','NKE','NOC','NOVN','NOW'
+,'NPACY','NRG','NSC','NTAP','NTES','NTRS','NUE','NVDA','NVR','NWL','NWSA'
+,'NXPI','NXPRF','O','ODFL','OKE','OKTA','OMC','ON','OR','ORA','ORCL'
+,'ORLY','OXY','PANW','PARA','PAYC','PAYX','PCAR','PCG','PDD','PEAK','PEG'
+,'PEP','PFE','PFG','PG','PGR','PH','PHM','PKG','PLD','PM','PNC','PNR'
+,'PNW','POOL','PPG','PPL','PRU','PSA','PSH','PSN','PSX','PTC','PWR','PXD'
+,'PYPL','QCOM','QRVO','RAND','RBSFY','RCL','RE','REG','REGN','RF','RHI'
+,'RIO','RJF','RL','RMD','ROG','ROK','ROL','ROP','ROST','RS','RSG','RTO'
+,'SAN','SAP','SBAC','SBNY','SBS','SBUX','SCHW','SEDG','SEE','SGEN','SHW'
+,'SIRI','SJM','SLB','SLOIF','SMIN','SNA','SNPS','SO','SPG','SPGI','SPLK'
+,'SRE','STLA','STM','STT','STX','STZ','SU','SVT','SWK','SWKS','SYF','SYK'
+,'SYY','T','TAP','TCS','TDG','TDY','TEAM','TECH','TEF','TEL','TER','TFC'
+,'TFX','TGT','TJX','TMO','TMUS','TPR','TRGP','TRMB','TROW','TRV','TSCO'
+,'TSLA','TSN','TT','TTWO','TXN','TXT','TYL','UAL','UDR','UHS','ULTA','UNH'
+,'UNP','UPS','URI','USB','UTG','V','VFC','VICI','VIV','VLO','VMC','VNO'
+,'VOD','VRBCF','VRSK','VRSN','VRTX','VTR','VTRS','VZ','WAB','WAT','WBA'
+,'WBD','WDAY','WDC','WEC','WELL','WFC','WHR','WM','WMB','WMT','WNDLF'
+,'WPP','WRB','WRK','WST','WY','WYNN','XEL','XOM','XRAY','XYL','YUM','ZBH'
+,'ZBRA','ZION','ZM','ZS','ZTS'])
